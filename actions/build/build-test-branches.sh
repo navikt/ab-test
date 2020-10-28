208e7f72205c68a7eed87ba9f6089c9b2e23ed0b
Julian Torp <julian.torp@nav.no>
2020-10-28 20:05:37 +0100

    correct build-test-branches script

--- actions/build/build-test-branches.sh
+++ actions/build/build-test-branches.sh
@@ -2,14 +2,14 @@
 
 git branch -r | grep --line-buffered $TEST_BRANCH_PREFIX | awk -v tbp=$TEST_BRANCH_PREFIX '{n=split($0,a, tbp); print a[n]}' | xargs -L1 >> ab-test-branches.tmp
 
-if [ $(sed -n '=' ab-test-branches.tmp | wc -l) -gt 0 ]; then
-  while read -r l;
+while read -r l;
+  if [ ! -z $l ]; then
     do git checkout "$TEST_BRANCH_PREFIX$l";
     npm install && npm run $BUILD_SCRIPT;
     mkdir tmp/"$l";
     cp -r $DIST_DIRECTORY/* tmp/"$l"/;
     git reset --hard;
-  done < ab-test-branches.tmp
-fi
+  fi
+done < ab-test-branches.tmp
 
 git checkout $GITHUB_REF
