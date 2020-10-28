f413bb2d3e21cc8969d9d6ec71c18392c5842719
Julian Torp <julian.torp@nav.no>
2020-10-28 20:11:07 +0100

    fix build-test-branches script

--- actions/build/build-test-branches.sh
+++ actions/build/build-test-branches.sh
@@ -2,9 +2,9 @@
 
 git branch -r | grep --line-buffered $TEST_BRANCH_PREFIX | awk -v tbp=$TEST_BRANCH_PREFIX '{n=split($0,a, tbp); print a[n]}' | xargs -L1 >> ab-test-branches.tmp
 
-while read -r l;
+while read -r l; do
   if [ ! -z $l ]; then
-    do git checkout "$TEST_BRANCH_PREFIX$l";
+    git checkout "$TEST_BRANCH_PREFIX$l";
     npm install && npm run $BUILD_SCRIPT;
     mkdir tmp/"$l";
     cp -r $DIST_DIRECTORY/* tmp/"$l"/;
